114076662b47069df461b155c1dcbaab6ca51f6d
Danny-jg16 <gallegosdaniela16@hotmail.com>
2019-09-29 14:27:23 -0500

    DG - Problem solution WEB-INF

--- MusicRecords/src/java/ec/edu/espe/ESPEmusic/connection/Connection.java
+++ MusicRecords/src/java/ec/edu/espe/ESPEmusic/connection/Connection.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ec.edu.espe.connection;
+package ec.edu.espe.ESPEmusic.connection;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
